5420d9405cccf596a6808045c1a6251332188766
LAPTOP-MT68H3K7\acer <Kanognipa26@gmail.com>
2023-01-07 13:56:29 +0700

    title name new 7-01-23

---
+++ app/src/main/java/com/a318/bothong65/Complaints.kt
@@ -0,0 +1,11 @@
+package com.a318.bothong65
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Complaints : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complaints)
+    }
+}
---
+++ app/src/main/java/com/a318/bothong65/Followup.kt
@@ -0,0 +1,11 @@
+package com.a318.bothong65
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Followup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_followup)
+    }
+}
